a7117d47f4db1ed4f9935025cd959a5d6b914ec5
Lucas pimentel <lucas47b@hotmail.com>
2025-04-24 21:31:02 -0300

    colocando contando no inicio da contagem

--- script.js
+++ script.js
@@ -9,30 +9,31 @@ function contar() {
     var num2 = Number(fim.value);
     var p = Number(passo.value);
 
-    if (num1=="" && num2=="" && p == ""){
+    if (num1 == "" && num2 == "" && p == "") {
         alert("insira um número")
     }
 
-    if (num1=="0" || num2=="0" || p == "0"){
+    if (num1 == "0" || num2 == "0" || p == "0") {
         alert("zero não é valido")
     }
 
-    resultado.innerHTML ="" //limpar o resultado anterior
+    resultado.innerHTML = "" //limpar o resultado anterior
 
-    if(num1<num2){
-        
+    if (num1 < num2) {
+        resultado.innerHTML = "contando: "
         //contagem crescente
-    while (num1 <= num2) {
+        while (num1 <= num2) {
 
-        resultado.innerHTML = resultado.innerHTML + "👉"+ num1 //auto atribuição, a cada loop ele mostra o resultado da atribuição de num1 += p
-        num1+=p // auto atribuição simplificada
-        /*estou atribuindo ao valor de num1 = num1 + p para que a cada loop seja adicionado o valor que esta em p*/
-       
-    }
+            resultado.innerHTML = resultado.innerHTML + "👉" + num1 //auto atribuição, a cada loop ele mostra o resultado da atribuição de num1 += p
+            num1 += p // auto atribuição simplificada
+            /*estou atribuindo ao valor de num1 = num1 + p para que a cada loop seja adicionado o valor que esta em p*/
 
-    }else{
-        
-        for(var num = num1;num>=num2;num-=p/*esse num-= vai decrementar no caso vai diminuir o valor de num de acordo com o numero colocado em p*/ ){
+        }
+
+    } else {
+
+        resultado.innerHTML = "contando: "
+        for (var num = num1; num >= num2; num -= p/*esse num-= vai decrementar no caso vai diminuir o valor de num de acordo com o numero colocado em p*/) {
 
             resultado.innerHTML += `${num} 👉`
 
@@ -40,8 +41,8 @@ function contar() {
     }
 
 
-    
+
 
     resultado.innerHTML = resultado.innerHTML + "🏁"
-   
+
 }
